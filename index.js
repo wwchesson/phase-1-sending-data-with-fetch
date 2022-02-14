318c5c731d8d7325d196f2c59d02bb9884c08222
Will <willchesson@gmail.com>
2022-02-14 17:59:40 -0500

    commit tree 8e14d055bb81b30a38910e5e4ac1fac9b32f8c75

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Add your code here
+function submitData(name, email) {
+    return fetch("http://localhost:3000/users", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json",
+        },
+        body: JSON.stringify( {
+            name, 
+            email
+        })
+    })
+    .then(res => res.json())
+    .then(object => document.body.innerHTML = object["id"])
+    .catch(error => document.body.innerHTML = error.message)
+}
